cc06c4f252cb99cee5e77b51d904cc8b8b6206df
Paul Gege <pgege@macbookpro.home>
2025-10-12 09:36:33 -0400

    update viteconfig

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,6 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
   server: {
-    host: true,
+    allowedHosts: true,
   },
 });
